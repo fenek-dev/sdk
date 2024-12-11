e8fd04ef9c13b0249dada8c0d2531dfca18d430c
fenek-dev <makstreonin@gmail.com>
2024-12-11 18:24:30 +0300

    add logger pkg

---
+++ pkg/logger/logger.go
@@ -0,0 +1,43 @@
+package logger
+
+import (
+	"io"
+	"log/slog"
+	"os"
+	"time"
+
+	"github.com/lmittmann/tint"
+)
+
+type Config struct {
+	Level slog.Level `env:"LOG_LEVEL" envDefault:"DEBUG"`
+}
+
+func Init(cfg *Config) *slog.Logger {
+	var (
+		log *slog.Logger
+		w   = os.Stdout
+	)
+	switch cfg.Level {
+	case slog.LevelDebug:
+		log = slog.New(tint.NewHandler(w, &tint.Options{
+			Level:      slog.LevelDebug,
+			TimeFormat: time.TimeOnly,
+		}))
+	case slog.LevelInfo, slog.LevelError, slog.LevelWarn:
+		log = slog.New(
+			slog.NewJSONHandler(w, &slog.HandlerOptions{Level: cfg.Level}),
+		)
+
+	default:
+		log = slog.New(
+			slog.NewJSONHandler(w, &slog.HandlerOptions{Level: slog.LevelInfo}),
+		)
+	}
+
+	return log
+}
+
+func NewDiscard() *slog.Logger {
+	return slog.New(slog.NewJSONHandler(io.Discard, nil))
+}
